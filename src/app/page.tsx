ccddf2de6b88801e78b1dfb12c096651db267397
Ivan Mijic <ivan@epifront.com>
2024-08-01 13:07:40 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Home() {
+  return (
+    <section id="hero__section" className="section">
+      <div className="container is-fluid">
+        NEXTjs && BULMA STARTER KIT
+      </div>
+    </section>
+  );
+}
